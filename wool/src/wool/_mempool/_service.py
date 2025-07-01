1e54b8c65846d41b5c52c60f12d61515eacf9766
Conrad <conradbzura@gmail.com>
2025-07-01 14:19:01 -0400

    refactor: Clean up mempool.proto protobuf definitions and update type hints

--- wool/src/wool/_mempool/_service.py
+++ wool/src/wool/_mempool/_service.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-import pickle
+from typing import AsyncGenerator
 from typing import Final
 from weakref import WeakSet
 from weakref import WeakValueDictionary
@@ -21,7 +21,7 @@ class Session:
     """
 
     id: Final[str]
-    queue: Final[asyncio.Queue]
+    queue: Final[asyncio.Queue[proto.SessionResponse]]
     references: Final[set[Reference]]
     sessions: Final[WeakValueDictionary[str, Session]] = WeakValueDictionary()
 
@@ -35,7 +35,7 @@ class Session:
         self.references = set()
         self.sessions[self.id] = self
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, Session):
             return self.id == other.id
         return False
@@ -55,25 +55,25 @@ class Reference:
     _to_delete: Final[set[str]] = set()
 
     @classmethod
-    def new(cls, id: str, *, mempool) -> Reference:
+    def new(cls, id: str, *, mempool: MemoryPool) -> Reference:
         if id in cls._references:
             raise ValueError(f"Session {id} already exists")
         return cls(id, mempool=mempool)
 
-    def __new__(cls, id: str, *, mempool):
+    def __new__(cls, id: str, *, mempool: MemoryPool):
         if id in cls._references:
             if id in cls._to_delete:
                 cls._to_delete.remove(id)
             return cls._references[id]
         return super().__new__(cls)
 
-    def __init__(self, id: str, *, mempool):
+    def __init__(self, id: str, *, mempool: MemoryPool):
         self.id = id
         self.mempool = mempool
         self.sessions = WeakSet()
         self._references[id] = self
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, Reference):
             return self.id == other.id
         return False
@@ -109,15 +109,15 @@ class MemoryPoolService(rpc.MemoryPoolServicer):
 
     async def session(
         self, request: proto.SessionRequest, context: ServicerContext
-    ):
+    ) -> AsyncGenerator[proto.SessionResponse]:
         session = Session()
         yield proto.SessionResponse(session=proto.Session(id=session.id))
         while True:
             yield await session.queue.get()
 
     async def acquire(
-        self, request: proto.Request, context: ServicerContext
-    ) -> proto.Response:
+        self, request: proto.AcquireRequest, context: ServicerContext
+    ) -> proto.AcquireResponse:
         if not (session := Session.get(request.session.id)):
             raise ValueError(f"Session {request.session.id} not found")
         await self._mempool.map(request.reference.id)
@@ -127,52 +127,59 @@ class MemoryPoolService(rpc.MemoryPoolServicer):
                 mempool=self._mempool,
             )
         )
-        return proto.Response(dump=b"")
+        return proto.AcquireResponse()
 
     async def put(
-        self, request: proto.Put, context: ServicerContext
-    ) -> proto.Response:
-        dump = b""
-        dump = pickle.dumps(
-            await self._mempool.put(request.dump, mutable=request.mutable)
-        )
-        return proto.Response(dump=dump)
-
-    async def post(
-        self, request: proto.Post, context: ServicerContext
-    ) -> proto.Response:
-        dump = b""
-        dump = pickle.dumps(
-            await self._mempool.post(request.reference.id, request.dump)
-        )
-        for session in Reference(
-            id=request.reference.id, mempool=self._mempool
-        ).sessions:
-            if session.id is not request.session.id:
-                await session.queue.put(
-                    proto.Event(
-                        reference=request.reference,
-                        event_type="post",
-                    )
-                )
-        return proto.Response(dump=dump)
-
-    async def get(
-        self, request: proto.Request, context: ServicerContext
-    ) -> proto.Response:
-        dump = await self._mempool.get(request.reference.id)
-        return proto.Response(dump=dump)
-
-    async def release(
-        self, request: proto.Request, context: ServicerContext
-    ) -> proto.Response:
-        dump = pickle.dumps(request.reference.id)
+        self, request: proto.PutRequest, context: ServicerContext
+    ) -> proto.PutResponse:
         if not (session := Session.get(request.session.id)):
             raise ValueError(f"Session {request.session.id} not found")
-        session.references.remove(
+        reference = Reference(
+            id=await self._mempool.put(request.dump, mutable=request.mutable),
+            mempool=self._mempool,
+        )
+        session.references.add(
             Reference(
-                id=request.reference.id,
+                id=reference.id,
                 mempool=self._mempool,
             )
         )
-        return proto.Response(dump=dump)
+        return proto.PutResponse(reference=proto.Reference(id=reference.id))
+
+    async def post(
+        self, request: proto.PostRequest, context: ServicerContext
+    ) -> proto.PostResponse:
+        if not (session := Session.get(request.session.id)):
+            raise ValueError(f"Session {request.session.id} not found")
+        updated = await self._mempool.post(request.reference.id, request.dump)
+        if updated:
+            for session in Reference(
+                id=request.reference.id, mempool=self._mempool
+            ).sessions:
+                if session.id is not request.session.id:
+                    event = proto.Event(
+                        reference=request.reference,
+                        event_type="post",
+                    )
+                    await session.queue.put(proto.SessionResponse(event=event))
+        return proto.PostResponse(updated=updated)
+
+    async def get(
+        self, request: proto.GetRequest, context: ServicerContext
+    ) -> proto.GetResponse:
+        if not Session.get(request.session.id):
+            raise ValueError(f"Session {request.session.id} not found")
+        dump = await self._mempool.get(request.reference.id)
+        return proto.GetResponse(dump=dump)
+
+    async def release(
+        self, request: proto.ReleaseRequest, context: ServicerContext
+    ) -> proto.ReleaseResponse:
+        if not (session := Session.get(request.session.id)):
+            raise ValueError(f"Session {request.session.id} not found")
+        reference = Reference(
+            id=request.reference.id,
+            mempool=self._mempool,
+        )
+        session.references.remove(reference)
+        return proto.ReleaseResponse()
